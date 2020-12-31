7a95a237fe8ea02a758db06debc83333f2fa837c
xiao <495303648@qq.com>
2020-12-31 11:01:39 +0800

    修复下载工具类，无法监听进度的问题

--- SmallOkHttp/src/main/java/com/smallcake/okhttp/DownloadUtils.java
+++ SmallOkHttp/src/main/java/com/smallcake/okhttp/DownloadUtils.java
@@ -26,6 +26,7 @@ import okhttp3.Response;
  * <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
  * <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
  * 使用'me.jessyan:progressmanager:1.5.0'监听真实的下载进度
+ * implementation 'me.jessyan:progressmanager:1.5.0'
  */
 
 public class DownloadUtils {
@@ -40,10 +41,10 @@ public class DownloadUtils {
 
     private DownloadUtils() {
         this.mHandler = new Handler(Looper.getMainLooper());
-        okHttpClient = SmallOkHttp.getOkHttpClient();
+        okHttpClient = ProgressManager.getInstance().with(new OkHttpClient.Builder()).build();
     }
 
-    public interface OnDownloadListener{
+    public interface OnDownloadListener {
         /**
          * 下载成功
          */
@@ -67,12 +68,10 @@ public class DownloadUtils {
      */
     public void download(final String url, final String saveDir, final String saveName, final OnDownloadListener listener) {
         Request request = new Request.Builder().url(url).build();
-
-
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.e("下载失败",e.getMessage());
+                Log.e("下载失败", e.getMessage());
                 // 下载失败
                 mHandler.post(new Runnable() {
                     @Override
@@ -140,5 +139,81 @@ public class DownloadUtils {
             }
         });
     }
+
+//    /**
+//     *
+//     * @param activity 上下文
+//     * @param down_url 下载链接
+//     * @param content 更新描述
+//     * @param isMust 是否强制更新
+//     * R.layout.dialog_update 确定框的布局，具体看XPopup
+//     */
+//    public static void showAppUpdate(Activity activity, String down_url, String content,boolean isMust) {
+//        new XPopup.Builder(activity)
+//                .dismissOnBackPressed(false)
+//                .dismissOnTouchOutside(false)
+//                .asConfirm("发现新版本",
+//                        content,
+//                        "稍后更新",
+//                        "立即更新",
+//                        () -> {
+//                            downloadApk(activity, down_url);
+//                        },
+//                        null,
+//                        isMust, R.layout.dialog_update)
+//                .show();
+//    }
+
+//    private static void downloadApk(Activity context, String down_url) {
+//        ProgressDialog progressDialog = new ProgressDialog(context);
+//        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+//        progressDialog.setCancelable(false);
+//        progressDialog.setCanceledOnTouchOutside(false);
+//        progressDialog.setMessage("更新进度");
+//        progressDialog.show();
+//        String path = context.getExternalCacheDir().getAbsolutePath() + File.separator;
+//        String name = "jiangxin.apk";
+//        DownloadUtils.getInstance().download(down_url, path, name, new DownloadUtils.OnDownloadListener() {
+//            @Override
+//            public void onDownloadSuccess() {
+//                ToastUtil.showShort("恭喜你下载成功，开始安装！");
+//                String successDownloadApkPath = path + name;
+//                AppUtils.installApk(context, successDownloadApkPath);
+//                new Handler().postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        ActivityCollector.finishAll();
+//                    }
+//                }, 3000);
+//            }
+//
+//            @Override
+//            public void onDownloading(ProgressInfo progressInfo) {
+//                progressDialog.setProgress(progressInfo.getPercent());
+//                boolean finish = progressInfo.isFinish();
+//                if (!finish) {
+//                    long speed = progressInfo.getSpeed();
+//                    progressDialog.setMessage("下载速度(" + (speed > 0 ? FormatUtils.formatSize(speed) : speed) + "/s)");
+//                } else {
+//                    progressDialog.setMessage("下载完成！");
+//                }
+//            }
+//
+//            @Override
+//            public void onDownloadFailed() {
+//                progressDialog.dismiss();
+//                context.runOnUiThread(() -> new XPopup.Builder(context)
+//                        .isDestroyOnDismiss(false)
+//                        .dismissOnTouchOutside(false)
+//                        .asConfirm("下载出错", "请重启应用!",
+//                                () -> ActivityCollector.finishAll(),
+//                                () -> ActivityCollector.finishAll()
+//                        ).show());
+//
+//
+//            }
+//        });
+//
+//    }
 
 }
